c2df3c642723eb90f3793e1026940a460219ccfc
Evan Boehs <ebrobot2000@gmail.com>
2021-02-26 15:05:17 -0500

    do some stuff lol

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 # region: setup
-# IMPORT DISCORD.PY. ALLOWS ACCESS TO DISCORD'S API.
 import discord
-# IMPORT THE OS MODULE.
 import os
-# IMPORT LOAD_DOTENV FUNCTION FROM DOTENV MODULE.
-# IMPORT COMMANDS FROM THE DISCORD.EXT MODULE.
+from dotenv import load_dotenv
+load_dotenv()
 from discord.ext import commands
-# GRAB THE API TOKEN FROM THE .ENV FILE.
+from discord.ext.commands import has_permissions, CheckFailure
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-# CREATES A NEW BOT OBJECT WITH A SPECIFIED PREFIX. IT CAN BE WHATEVER YOU WANT IT TO BE.
 bot = commands.Bot(command_prefix="&")
+import random
+import asyncio
+
+birdfacts = ["Lucy's warbler is the smallest known species of warbler!", "my pfp came from <https://www.drawingtenthousandbirds.com/new-blog-1/2015/4/18/why-a-yellow-warbler>", "There are 119 species of warbler"]
 
 wordblacklist = open("config/blacklist.txt","r")
 wordblacklist = wordblacklist.readlines()
@@ -71,11 +72,14 @@ async def tiers(offender, tier, reason):
   # Permaban
     return
 
+bot.remove_command("help")
+
 @bot.event
 async def on_ready():
   print(f'> Logged in as: {bot.user}')
   print(bot.user.id)
   print('-----READY----- \n')
+  await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="The chat! do &help to learn more about my functions"))
 
 @bot.event
 async def on_message(message):
@@ -88,13 +92,38 @@ async def on_message(message):
   for word in wordgraylist:
     if word in message.content:
       await message.delete()
+  mention = f'<@!{bot.user.id}>'
+  if mention in message.content:
+    users = []
+    role = discord.utils.get(message.guild.roles,name="Helper")
+    for member in message.guild.members:
+      memberis = member.id
+      if role in member.roles: # does member have the specified role?
+        print(memberis)
+        if member.status == discord.Status.online:
+          print(memberis)
+          users.append(memberis)
+    await message.channel.send("⚠️ " + message.author.mention + " Mentioned me!")
+    print([f"<@!{i}>" for i in users])
+    await message.channel.send([f"<@!{i}>" for i in users])
+  await bot.process_commands(message)
 
 @bot.command()
 async def checkpoints(ctx, user):
-	await ctx.channel.send("eventually this will check points")
+  async with ctx.typing():
+    # do expensive stuff here
+    await asyncio.sleep(1)
+  await ctx.channel.send("**Hey! 👋** Give me a sec while I look that up for you!")
 
 @bot.command()
-@commands.check(ban_members=True)
+async def help(ctx):
+  async with ctx.typing():
+    # do expensive stuff here
+    await asyncio.sleep(1)
+  await ctx.channel.send("**Hey! 👋** \nmy job is mostly to help keep the chat clean, and give our wonderful helpers a hand, but there is some useful stuff you should know!\n**Ping Me** if something needs immediate (like right right right now) attention\n**DM me** to contact the mods if something needs private attention\n\n**Oh!** One last thing. here is a random bird fact!\n> " + random.choice(birdfacts))
+
+@bot.command()
+@commands.has_role("Helper")
 async def point(ctx, ammount, user, *, reason):
 	await ctx.channel.send("eventually this will grant a point")
 
